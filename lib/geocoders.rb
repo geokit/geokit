caae0eab81dc1b64b34bdd0ca5c47df6da33a7e9
Andre Lewis <andre@highgroove.com>
2008-12-19 02:48:54 -0800

    added geonames support

--- lib/geocoders.rb
+++ lib/geocoders.rb
@@ -59,11 +59,12 @@ module Geokit
     @@google = 'REPLACE_WITH_YOUR_GOOGLE_KEY'
     @@geocoder_us = false
     @@geocoder_ca = false
+    @@geonames = false
     @@provider_order = [:google,:us]
     @@logger=Logger.new(STDOUT)
     @@logger.level=Logger::INFO
     
-    [:yahoo, :google, :geocoder_us, :geocoder_ca, :provider_order, :timeout, 
+    [:yahoo, :google, :geocoder_us, :geocoder_ca, :geonames, :provider_order, :timeout, 
      :proxy_addr, :proxy_port, :proxy_user, :proxy_pass,:logger].each do |sym|
       class_eval <<-EOS, __FILE__, __LINE__
         def self.#{sym}
@@ -369,6 +370,54 @@ module Geokit
         rescue 
           logger.info "Caught an error during Yahoo geocoding call: "+$!
           return GeoLoc.new
+      end
+    end
+
+    class GeonamesGeocoder < Geocoder
+
+      private 
+      
+      # Template method which does the geocode lookup.
+      def self.do_geocode(address)
+        address_str = address.is_a?(GeoLoc) ? address.to_geocodeable_s : address
+        # geonames need a space seperated search string
+        address_str.gsub!(/,/, " ")
+        params = "/postalCodeSearch?placename=#{Geokit::Inflector::url_escape(address_str)}&maxRows=10"
+        
+        if(GeoKit::Geocoders::geonames)
+          url = "http://ws.geonames.net#{params}&username=#{GeoKit::Geocoders::geonames}"
+        else
+          url = "http://ws.geonames.org#{params}"
+        end
+        
+        res = self.call_geocoder_service(url)
+        
+        return GeoLoc.new if !res.is_a?(Net::HTTPSuccess)
+        
+        xml=res.body
+        logger.debug "Geonames geocoding. Address: #{address}. Result: #{xml}"
+        doc=REXML::Document.new(xml)
+        
+        if(doc.elements['//geonames/totalResultsCount'].text.to_i > 0)
+          res=GeoLoc.new
+        
+          # only take the first result
+          res.lat=doc.elements['//code/lat'].text if doc.elements['//code/lat']
+          res.lng=doc.elements['//code/lng'].text if doc.elements['//code/lng']
+          res.country_code=doc.elements['//code/countryCode'].text if doc.elements['//code/countryCode']
+          res.provider='genomes'  
+          res.city=doc.elements['//code/name'].text if doc.elements['//code/name']
+          res.state=doc.elements['//code/adminName1'].text if doc.elements['//code/adminName1']
+          res.zip=doc.elements['//code/postalcode'].text if doc.elements['//code/postalcode']
+          res.success=true
+          return res
+        else 
+          logger.info "Geonames was unable to geocode address: "+address
+          return GeoLoc.new
+        end
+        
+        rescue
+          logger.error "Caught an error during Geonames geocoding call: "+$!
       end
     end
     
