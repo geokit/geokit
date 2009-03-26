e9c0a2e47084e09e05fa6e34c39bda3b73a65615
glennpow <glennpow@gmail.com>
2009-03-26 12:58:43 +0100

    Moved ActionController mixin to plugin

--- lib/geokit.rb
+++ lib/geokit.rb
@@ -25,7 +25,6 @@ path = File.expand_path(File.dirname(__FILE__))
 $: <<  path unless $:.include?(path)
 require 'geokit/geocoders'
 require 'geokit/mappable'
-require 'geokit/geocoder_control'
 
 # make old-style module name "GeoKit" equivalent to new-style "Geokit"
 GeoKit=Geokit
