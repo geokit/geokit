2072ddb4f2af3607ca3db6092a9dcffc295cc514
Andre Lewis <andre@highgroove.com>
2009-06-08 20:59:06 -0700

    version bump + history

--- lib/geokit.rb
+++ lib/geokit.rb
@@ -1,5 +1,5 @@
 module Geokit
-  VERSION = '1.3.2'
+  VERSION = '1.4.0'
   # These defaults are used in Geokit::Mappable.distance_to and in acts_as_mappable
   @@default_units = :miles
   @@default_formula = :sphere
