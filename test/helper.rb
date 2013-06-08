ee5434866d15f21a9d67faaca3c8c641b125aa0a
Michael Noack <michael.noack@travellink.com.au>
2013-06-08 19:16:34 +0930

    require rubygems/bundler so ruby 1.8 can load gems, e.g. mocha

--- test/helper.rb
+++ test/helper.rb
@@ -1,5 +1,13 @@
 # encoding: utf-8
 
+begin
+  require 'rubygems'
+  require 'bundler'
+  Bundler.setup
+rescue LoadError => e
+  puts "Error loading bundler (#{e.message}): \"gem install bundler\" for bundler support."
+end
+
 require 'test/unit'
 require 'mocha'
 require 'net/http'
