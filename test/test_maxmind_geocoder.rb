d6f12aca03ed7d6e4df654839b2e0638c9b3de0c
Jose Luis Salas <josacar@gmail.com>
2013-12-04 18:22:39 +0000

    Remove geoip blob

--- test/test_maxmind_geocoder.rb
+++ test/test_maxmind_geocoder.rb
@@ -8,10 +8,20 @@ class MaxmindGeocoderTest < BaseGeocoderTest #:nodoc: all
   end
 
   def test_ip
-    VCR.use_cassette('maxmind_ip') do
+    location = mock()
+    city = mock()
+    ::GeoIP.stubs(:new).with(anything).returns(location)
+    location.expects(:city).with(@ip).returns(city)
+    city.stubs(:latitude).returns(-34.9287)
+    city.stubs(:longitude).returns(138.5986)
+    city.stubs(:city_name).returns('Adelaide')
+    city.stubs(:region_name).returns('Australia')
+    city.stubs(:postal_code).returns('')
+    city.stubs(:country_code3).returns('AUS')
+
     res = Geokit::Geocoders::MaxmindGeocoder.geocode(@ip)
     assert_equal 'Adelaide', res.city
-    end
+    assert res.city
   end
 
 end
