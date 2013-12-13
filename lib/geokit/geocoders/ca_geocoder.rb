32d7047b4259ed7bd1dc316364b184c63fb6a383
Michael Noack <michael.noack@travellink.com.au>
2013-12-13 23:17:13 +1030

    Simplify url creation

--- lib/geokit/geocoders/ca_geocoder.rb
+++ lib/geokit/geocoders/ca_geocoder.rb
@@ -36,19 +36,15 @@ module Geokit
 
      # Formats the request in the format acceptable by the CA geocoder.
      def self.construct_request(location)
-       url = ""
-       url += add_ampersand(url) + "stno=#{location.street_number}" if location.street_address
-       url += add_ampersand(url) + "addresst=#{Geokit::Inflector::url_escape(location.street_name)}" if location.street_address
-       url += add_ampersand(url) + "city=#{Geokit::Inflector::url_escape(location.city)}" if location.city
-       url += add_ampersand(url) + "prov=#{location.state}" if location.state
-       url += add_ampersand(url) + "postal=#{location.zip}" if location.zip
-       url += add_ampersand(url) + "auth=#{Geokit::Geocoders::geocoder_ca}" if Geokit::Geocoders::geocoder_ca
-       url += add_ampersand(url) + "geoit=xml"
-       'http://geocoder.ca/?' + url
-     end
-
-     def self.add_ampersand(url)
-       url && url.length > 0 ? "&" : ""
+       args = []
+       args << "stno=#{location.street_number}" if location.street_address
+       args << "addresst=#{Geokit::Inflector::url_escape(location.street_name)}" if location.street_address
+       args << "city=#{Geokit::Inflector::url_escape(location.city)}" if location.city
+       args << "prov=#{location.state}" if location.state
+       args << "postal=#{location.zip}" if location.zip
+       args << "auth=#{Geokit::Geocoders::geocoder_ca}" if Geokit::Geocoders::geocoder_ca
+       args << "geoit=xml"
+       'http://geocoder.ca/?' + args.join('&')
      end
    end
  end
