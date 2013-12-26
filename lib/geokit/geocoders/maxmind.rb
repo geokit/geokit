653df50c91a40d984e3eababf21458a33fb62dcc
Michael Noack <michael.noack@travellink.com.au>
2013-12-27 01:17:55 +1030

    Configurize maxmind data path

--- lib/geokit/geocoders/maxmind.rb
+++ lib/geokit/geocoders/maxmind.rb
@@ -1,15 +1,13 @@
 module Geokit
   module Geocoders
-
-  @@geoip_data_path = 'DEFINE_THE_PATH_TO_GeoLiteCity.dat'
-  __define_accessors
-
     # Provides geocoding based upon an IP address.  The underlying web service is MaxMind
     class MaxmindGeocoder < Geocoder
+      config :geoip_data_path # path to GeoLiteCity.dat
+
       private
 
       def self.do_geocode(ip)
-        res = GeoIP.new(Geokit::Geocoders::geoip_data_path).city(ip)
+        res = GeoIP.new(geoip_data_path).city(ip)
 
         loc = new_loc
         loc.lat          = res.latitude
