04fbe768ab89e7f5ad3d0487028cf44553091191
Thiago Charão <charao.thiago@gmail.com>
2014-05-02 11:00:08 -0300

    Get.new(url) or Get.new(uri)
    In some envs creating a Get with url the response is not 200, using uri
    it doesn’t happen.

--- lib/geokit/net_adapter/net_http.rb
+++ lib/geokit/net_adapter/net_http.rb
@@ -3,7 +3,7 @@ module Geokit
     class NetHttp
       def self.do_get(url)
         uri = URI.parse(url)
-        req = Net::HTTP::Get.new(url)
+        req = Net::HTTP::Get.new(uri)
         req.basic_auth(uri.user, uri.password) if uri.userinfo
         net_http_args = [uri.host, uri.port]
         if (proxy_uri_string = Geokit::Geocoders::proxy)
