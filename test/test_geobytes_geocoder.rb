19620bdc05a7eaad29886e31c84d9f68911a4164
Erik Michaels-Ober <sferik@gmail.com>
2015-04-22 14:31:22 +0200

    Remove tailing whitespace

--- test/test_geobytes_geocoder.rb
+++ test/test_geobytes_geocoder.rb
@@ -4,11 +4,11 @@ class GeobytesGeocoderTest < BaseGeocoderTest #:nodoc: all
     super
     @ip = "12.12.12.12"
   end
-  
+
   def assert_url(expected_url)
     assert_equal expected_url, TestHelper.get_last_url.gsub(/&oauth_[a-z_]+=[a-zA-Z0-9\-. %]+/, "").gsub("%20", "+")
   end
-  
+
   def test_geobytes_geocoder
     VCR.use_cassette("geobytes_geocode") do
       url = "http://getcitydetails.geobytes.com/GetCityDetails?fqcn=#{@ip}"
