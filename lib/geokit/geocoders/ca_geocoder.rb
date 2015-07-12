d484c627f2b49ddae9cee73bd27a97d3329774dc
Luis Saffie <luis@homestars.com>
2015-07-11 22:37:57 -0400

    Fixes the CaGeocoder adapter
    reworked code to use the locate url param when submiting to geocoder.ca
    
    Also, when parsing xml, I'm writing all elements to the returned object,
    just like the other adapters

--- lib/geokit/geocoders/ca_geocoder.rb
+++ lib/geokit/geocoders/ca_geocoder.rb
@@ -18,25 +18,25 @@ module Geokit
 
       # Template method which does the geocode lookup.
       def self.do_geocode(loc)
-        raise ArgumentError("Geocoder.ca requires a GeoLoc argument") unless loc.is_a?(GeoLoc)
-        process :xml, submit_url(loc), loc
+        process :xml, submit_url(loc), GeoLoc.new
       end
 
       def self.parse_xml(xml, loc)
         loc.lat = xml.elements["//latt"].text
         loc.lng = xml.elements["//longt"].text
+        loc.city = xml.elements["//city"].text
+        loc.street_number = xml.elements["//stnumber"].text
+        loc.street_address = xml.elements["//staddress"].text
+        loc.state = xml.elements["//prov"].text
+        loc.province = xml.elements["//prov"].text
+        loc.zip = xml.elements["//postal"].text
         loc.success = true
         loc
       end
 
       # Formats the request in the format acceptable by the CA geocoder.
       def self.submit_url(loc)
-        args = []
-        args << "stno=#{loc.street_number}" if loc.street_address
-        args << "addresst=#{Geokit::Inflector.url_escape(loc.street_name)}" if loc.street_address
-        args << "city=#{Geokit::Inflector.url_escape(loc.city)}" if loc.city
-        args << "prov=#{loc.state}" if loc.state
-        args << "postal=#{loc.zip}" if loc.zip
+        args = ["locate=#{Geokit::Inflector.url_escape(loc)}"]
         args << "auth=#{key}" if key
         args << "geoit=xml"
         "http://geocoder.ca/?" + args.join("&")
--- test/test_ca_geocoder.rb
+++ test/test_ca_geocoder.rb
@@ -5,34 +5,37 @@ Geokit::Geocoders::CaGeocoder.key = "SOMEKEYVALUE"
 class CaGeocoderTest < BaseGeocoderTest #:nodoc: all
   CA_SUCCESS = <<-EOF
   <?xml version="1.0" encoding="UTF-8" ?>
-  <geodata><latt>49.243086</latt><longt>-123.153684</longt></geodata>
+  <geodata> <latt>49.243086</latt><longt>-123.153684</longt>
+  <postal>V6L2J7</postal> <standard> <stnumber>2105</stnumber>
+  <staddress>32nd AVE W</staddress><city>Vancouver</city><prov>BC</prov>
+  <confidence>0.8</confidence></standard> </geodata>
   EOF
 
   def setup
-    @ca_full_hash = {street_address: "2105 West 32nd Avenue", city: "Vancouver", state: "BC"}
-    @ca_full_loc = Geokit::GeoLoc.new(@ca_full_hash)
+    @ca_full_hash = { street_address: "2105 West 32nd Avenue",
+                      city: "Vancouver", province: "BC", state: "BC" }
+    @ca_full_txt = "2105 West 32nd Avenue Vancouver BC"
   end
 
   def test_geocoder_with_geo_loc_with_account
     response = MockSuccess.new
     response.expects(:body).returns(CA_SUCCESS)
-    url = "http://geocoder.ca/?stno=2105&addresst=West+32nd+Avenue&city=Vancouver&prov=BC&auth=SOMEKEYVALUE&geoit=xml"
+    url = "http://geocoder.ca/?locate=2105+West+32nd+Avenue+Vancouver+BC&auth=SOMEKEYVALUE&geoit=xml"
     Geokit::Geocoders::CaGeocoder.expects(:call_geocoder_service).with(url).returns(response)
-    verify(Geokit::Geocoders::CaGeocoder.geocode(@ca_full_loc))
+    verify(Geokit::Geocoders::CaGeocoder.geocode(@ca_full_txt))
   end
 
   def test_service_unavailable
     response = MockFailure.new
-    # Net::HTTP.expects(:get_response).with(URI.parse("http://geocoder.ca/?stno=2105&addresst=West+32nd+Avenue&city=Vancouver&prov=BC&auth=SOMEKEYVALUE&geoit=xml")).returns(response)
-    url = "http://geocoder.ca/?stno=2105&addresst=West+32nd+Avenue&city=Vancouver&prov=BC&auth=SOMEKEYVALUE&geoit=xml"
+    url = "http://geocoder.ca/?locate=2105+West+32nd+Avenue+Vancouver+BC&auth=SOMEKEYVALUE&geoit=xml"
     Geokit::Geocoders::CaGeocoder.expects(:call_geocoder_service).with(url).returns(response)
-    assert !Geokit::Geocoders::CaGeocoder.geocode(@ca_full_loc).success
+    assert !Geokit::Geocoders::CaGeocoder.geocode(@ca_full_txt).success
   end
 
   private
 
   def verify(location)
-    assert_equal "BC", location.state
+    assert_equal "BC", location.province
     assert_equal "Vancouver", location.city
     assert_equal "49.243086,-123.153684", location.ll
     assert !location.is_us?
