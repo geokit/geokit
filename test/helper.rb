7123442a09f454281be08446742e5b4b86bc6da3
Michael Noack <michael@noack.com.au>
2016-03-25 11:12:51 +1030

    Extract coverage into loader and turn on by default

--- test/helper.rb
+++ test/helper.rb
@@ -10,31 +10,7 @@ end
 
 require "geoip"
 
-if ENV["COVERAGE"]
-  COVERAGE_THRESHOLD = 95
-  require "simplecov"
-  require "simplecov-rcov"
-  require "coveralls"
-  Coveralls.wear!
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
-    SimpleCov::Formatter::RcovFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-  SimpleCov.start do
-    add_filter "/test/"
-    add_group "lib", "lib"
-  end
-  SimpleCov.at_exit do
-    SimpleCov.result.format!
-    percent = SimpleCov.result.covered_percent
-    unless percent >= COVERAGE_THRESHOLD
-      puts "Coverage must be above #{COVERAGE_THRESHOLD}%. It is #{'%.2f' % percent}%"
-      Kernel.exit(1)
-    end
-  end
-end
-
+require 'coverage_loader'
 require "test/unit"
 require "mocha/setup"
 require "net/http"
