0af756c45d566bc9e91e17bf9974c9b85b31d7df
Michael Noack <michael@noack.com.au>
2016-04-30 11:21:24 +0930

    Update mapbox test now key is required

--- test/test_mapbox_geocoder.rb
+++ test/test_mapbox_geocoder.rb
@@ -8,6 +8,7 @@ class MapboxGeocoderTest < BaseGeocoderTest #:nodoc: all
     @address = "1714 14th Street NW, Washington, DC"
     @latlng = Geokit::LatLng.new(38.913175, -77.032458)
     @city = "Washington, DC"
+    Geokit::Geocoders::MapboxGeocoder.key = "pk.eyJ1IjoiZ2Vva2l0IiwiYSI6ImNpbm1md3dvdTB6anB0dG0zbW9zd3hlODQifQ.dyskNcYK8DvxyjCO3i7afA"
   end
 
   def test_forward_geocode
@@ -33,8 +34,8 @@ class MapboxGeocoderTest < BaseGeocoderTest #:nodoc: all
   def test_city_only
     VCR.use_cassette("mapbox_forward_geocode_city_only") do
       res = Geokit::Geocoders::MapboxGeocoder.geocode(@city)
-      assert_equal 38.8951, res.lat
-      assert_equal(-77.0364, res.lng)
+      assert_equal 38.895, res.lat
+      assert_equal(-77.0366, res.lng)
       assert_equal "United States", res.country
       assert_equal "District of Columbia", res.state
       assert_equal "Washington", res.city
